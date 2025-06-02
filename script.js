6bdf570bb349add6354b17741abb8b82f3bc3409
Dev-YoungPandas <dev@youngpandas.com>
2025-06-02 22:34:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputBox = document.getElementById('input-box');
+const list = document.getElementById('list-container');
+
+
+function addTask(event) {
+
+    event.preventDefault();
+
+    if (inputBox.value === ""){
+        alert("please add somthing")
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+
+    inputBox.value = " "
+}
+
+
+list.addEventListener("click", function (e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+document.querySelector(".add").addEventListener("click", function (e) {
+
+    addTask(e);
+    saveData();
+
+});
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
